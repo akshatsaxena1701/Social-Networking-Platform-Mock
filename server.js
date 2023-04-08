093c997262936e4d69f06c496ecf94bdfa8f099e
Akshat Saxena <akshatsaxena@Akshats-MacBook-Air.local>
2023-04-08 06:49:30 +0530

    Profiles

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express=require('express')
 const app=express()
 const PORT=process.env.PORT || 4000
 const connectDB=require("./config/db");
-const path = require('path')
 connectDB();
 
 app.use(express.json({extended:false}));
@@ -13,12 +12,9 @@ app.use('/api/auth',require('./routes/api/auth'));
 app.use('/api/profile',require('./routes/api/profile'));
 app.use('/api/post',require('./routes/api/post'));
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('frontend/build'));
-    app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
-    })
-}
+app.get('/',(req,res)=>{
+    res.send("Server is running");
+})
 
 app.listen(PORT,()=>{
     console.log(`server started on port ${PORT}`)
